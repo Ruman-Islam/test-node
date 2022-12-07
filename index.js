d8ff821a6cd370c11550a5addfa2a3bd043be54b
Ruman-Islam <rumanislam48@gmail.com>
2022-12-08 02:17:17 +0600

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 // set the view engine to ejs
-app.use(express.static("public"));
+app.use(express.static("views"));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
